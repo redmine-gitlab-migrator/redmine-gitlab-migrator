9feeff92e1122254fe9e75795749eff253d1ccaa
Jocelyn Delalande <jdelalande@oasiswork.fr>
2015-11-30 10:56:55 +0100

    Bump to v1.0.1 and changed pkg name
    For pypi release

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(
-    name='migrate-redmine-to-gitlab',
-    version='1.0',
+    name='redmine-gitlab-migrator',
+    version='1.0.1',
     description='Migrate a redmine project to gitlab',
     long_description=README,
     author='Jocelyn Delalande',
