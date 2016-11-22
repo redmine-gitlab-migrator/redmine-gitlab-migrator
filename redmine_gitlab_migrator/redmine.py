294cad2c0948004b7b378fcad615a25b8972279d
Johan Vervloet <johanv@johanv.org>
2016-11-22 13:51:21 +0100

    Revert "Map user without user name to root as well."
    This reverts commit 0a71e3c17941d45d595c6c9997d07112226ec929.
    
    User without a user name is not the problem. But in our issue tracker,
    some issues were assigned to the anonymous user, and that was
    something the migrator did not like.

--- redmine_gitlab_migrator/redmine.py
+++ redmine_gitlab_migrator/redmine.py
@@ -113,7 +113,7 @@ class RedmineProject(Project):
             if i != ANONYMOUS_USER_ID:
                 user = self.api.get('{}/users/{}.json'.format(
                     self.instance_url, i))
-                if (user['login'] == 'admin') or not user['login']:
+                if user['login'] == 'admin':
                     user['login'] = 'root'
                 users.append(user)
         return users
