06cfade4baba38a381fa2721fad16a290d7dd6a6
Florian Fassing <ffassing@mediengarage.de>
2020-07-10 15:53:41 +0200

    fixed issue where only 'author' was checked when 'name' was present though, added support for estimated and spent time

--- redmine_gitlab_migrator/gitlab.py
+++ redmine_gitlab_migrator/gitlab.py
@@ -187,6 +187,12 @@ class GitlabProject(Project):
                 issue_notes_url, data=note_data,
                 headers=note_headers)
 
+        # Handle estimated and spent time
+        time_estimate_url = '{}/time_estimate?duration={}'.format(issue_url, meta['human_time_estimate'])
+        self.api.post(time_estimate_url)
+        time_spent_url = '{}/add_spent_time?duration={}'.format(issue_url, meta['human_total_time_spent'])
+        self.api.post(time_spent_url)
+
         # Handle closed status
         if meta['must_close']:
             self.api.put(issue_url, {'state_event': 'close'})
